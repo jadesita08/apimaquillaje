d30aeb439ee1ba2093285befa6592de5b590cccb
jadesita08 <jadeorona630@gmail.com>
2025-11-18 22:06:53 -0300

    Update ventasManager.py

--- managers/ventasManager.py
+++ managers/ventasManager.py
@@ -1,5 +1,6 @@
+import psycopg
 from managers.conexionManager import ConexionManager
-from models.models import Venta
+from models.models import Venta 
 
 class VentasManager:
     def __init__(self):
@@ -7,7 +8,6 @@ class VentasManager:
     
     def crear_venta(self, venta: Venta):
         conn = self.conn_manager.get_connection()
-        
         cursor = conn.cursor()
         cursor.execute(
             "INSERT INTO ventas (cliente_id, producto_id, cantidad, total) VALUES (%s, %s, %s, %s) RETURNING id",
@@ -21,26 +21,34 @@ class VentasManager:
     
     def obtener_ventas(self):
         conn = self.conn_manager.get_connection()
-
         cursor = conn.cursor()
         cursor.execute("SELECT id, cliente_id, producto_id, cantidad, total, fecha_venta FROM ventas")
-        
         column_names = [desc[0] for desc in cursor.description]
-        
         ventas = []
         for row in cursor.fetchall():
             venta_dict = dict(zip(column_names, row))
             if 'fecha_venta' in venta_dict and venta_dict['fecha_venta'] is not None:
                  venta_dict['fecha_venta'] = venta_dict['fecha_venta'].isoformat()
             ventas.append(venta_dict)
-            
         cursor.close()
         conn.close()
         return ventas
     
+    def actualizar_venta(self, venta_id: int, venta: Venta):
+        conn = self.conn_manager.get_connection()
+        cursor = conn.cursor()
+        cursor.execute(
+            "UPDATE ventas SET cliente_id = %s, producto_id = %s, cantidad = %s, total = %s WHERE id = %s",
+            (venta.cliente_id, venta.producto_id, venta.cantidad, venta.total, venta_id)
+        )
+        updated_rows = cursor.rowcount
+        conn.commit()
+        cursor.close()
+        conn.close()
+        return updated_rows > 0
+
     def eliminar_venta(self, venta_id: int):
         conn = self.conn_manager.get_connection()
-        
         cursor = conn.cursor()
         cursor.execute("DELETE FROM ventas WHERE id = %s", (venta_id,))
         deleted_rows = cursor.rowcount
